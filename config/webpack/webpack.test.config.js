e3f96cde1fa351e8579d8c5dd21dd1e6b86153f1
JOSEPH PUZZO <joseph0096.puzzo@libertymutual.com>
2018-01-09 15:00:32 -0500

    added test stuff

---
+++ config/webpack/webpack.test.config.js
@@ -0,0 +1,26 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  devtool: 'source-map',
+  target: 'node', // in order to ignore built-in modules like path, fs, etc.
+  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+  output: {
+    devtoolModuleFilenameTemplate: '[absolute-resource-path]',
+    devtoolFallbackModuleFilenameTemplate: '[absolute-resource-path]?[hash]'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader?modules=true&localIdentName=[name]__[local]___[hash:base64:5]', 'sass-loader'],
+      },
+      {
+        test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)|\.png($|\?)|\.jpg($|\?)/,
+        loader: 'url-loader',
+      }
+    ],
+  }
+};
